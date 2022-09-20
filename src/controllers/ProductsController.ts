c72edc44d4b3e5817fa7100c638a8d3cfd327678
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-09-20 08:37:32 -0300

    controller

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -11,12 +11,12 @@ export class ProductController {
   
   async index (req: Request, res: Response): Promise<Response> {
     const result = await this.getProductsUseCase.execute()
-    return res.json({ user: result }).status(200)
+    return res.json({ products: result }).status(200)
   }
 
   async create (req: Request, res: Response): Promise<Response> {
     const { slug } = req.body
     const result = await this.createProductUseCase.execute({ slug })
-    return res.json({ user: result }).status(201)
+    return res.json({ products: result }).status(201)
   }
 }
