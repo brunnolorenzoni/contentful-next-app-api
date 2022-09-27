0af50692f9e2b267fb84b4eb7978fbba15768ac6
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-09-27 10:39:01 -0300

    multer

--- src/routes/assets.ts
+++ src/routes/assets.ts
@@ -4,10 +4,8 @@ import validateRequestSchema from "../middlewares/ValidateRequestSchema"
 import { createAssetSchema, fileSchema } from "../schema/joi-validator/Assets"
 import multer from 'multer'
 
-const upload = multer()
-
 const assetsRoute = Router()
 
-assetsRoute.post('/', upload.single('file'), validateRequestSchema('file', fileSchema), validateRequestSchema('body', createAssetSchema), (request: Request, response: Response, next: NextFunction) => assetsController.create(request, response, next))
+assetsRoute.post('/', multer().single('file'), validateRequestSchema('file', fileSchema), validateRequestSchema('body', createAssetSchema), (request: Request, response: Response, next: NextFunction) => assetsController.create(request, response, next))
 
 export default assetsRoute
