28acb9923bd81b7e5c0cc3fff44edf2ad13d0b76
Brunno Lorenzoni <brunno.lorenzoni@unicred.com.br>
2022-09-30 11:54:01 -0300

    setup jest test

---
+++ tests/useCases/createProduct.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe('Create Product', () => {
+  test('create with ...', () => {
+    const x = true
+    expect(x).toBe(true)
+  });
+});
